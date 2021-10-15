4a88520ab836aab86cdca30d0e3dc14897980b6a
Miguel Vila <miguelovila@ua.pt>
2021-10-15 10:24:09 +0100

    Committing 8 files

---
+++ fundamentos_de_programacao/aulas_teoricas/aula06/turtledraw.py
@@ -0,0 +1,14 @@
+# Exercise 5 on "How to think like a computer scientist", ch. 11.
+
+import turtle
+
+t = turtle.Turtle()
+
+# Use t.up(), t.down() and t.goto(x, y)
+
+# Put your code here
+...
+
+# wait
+turtle.Screen().exitonclick()
+
