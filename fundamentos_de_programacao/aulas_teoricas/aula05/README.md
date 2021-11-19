aa942c087dd09e985455eee6ca29ece23f7ff5d2
Miguel Vila <miguelovila@ua.pt>
2021-11-19 20:01:45 +0000

    Committing 8 files

---
+++ fundamentos_de_programacao/aulas_teoricas/aula05/README.md
@@ -0,0 +1,22 @@
+# Bibliografia para a aula 05.
+
+* Slides e guião disponibilizados nesta pasta.
+* [py4e] cap. 9, cap. 7, sec. 11.1--11.3 e 11.8--11.10
+* Ou [1] caps. 9 e 10
+* Ou [2] caps. 8, 10 e 12
+
+[py4e] [Python for Everybody: Interactive](https://elearning.ua.pt/mod/lti/view.php?id=1137743)
+
+[1] [How to Think Like a Computer Scientist: Interactive Edition](https://runestone.academy/runestone/static/thinkcspy/index.html)
+
+[2] [Think Python 2e](http://greenteapress.com/wp/think-python-2e/)
+
+Nos livros interativos, recomenda-se:
+
+* Ler cada capítulo até à secção "Glossary".
+* Responder às questões no texto.
+* Estudar exemplos que estão em caixas "Codelens".
+  Estas caixas permitem executar as instruções passo-a-passo e visualizar a
+  evolução das variáveis na memória à medida que o programa é executado.
+* Para aprofundar, pode fazer alguns exercícios que aparecem no fim do capítulo.
+
---
+++ fundamentos_de_programacao/aulas_teoricas/aula05/telephones.py
@@ -0,0 +1,35 @@
+
+# Convert a telephone number into corresponding name, if on list.
+# (If not on list, just return the number itself.)
+def telToName(tel, telList, nameList):
+    # your code here
+    ...
+    return name
+
+
+# Return list of telephone numbers corresponding to names containing partName.
+def nameToTels(partName, telList, nameList):
+    # your code here
+    ...
+    return tels
+
+def main():
+    # Lists of telephone numbers and names
+    telList = ['975318642', '234000111', '777888333', '911911911']
+    nameList = ['Angelina', 'Brad',      'Claudia',   'Bruna']
+
+    # Test telToName:
+    tel = input("Tel number? ")
+    print( telToName(tel, telList, nameList) )
+    print( telToName('234000111', telList, nameList) == "Brad" )
+    print( telToName('222333444', telList, nameList) == "222333444" )
+
+    # Test nameToTels:
+    name = input("Name? ")
+    print( nameToTels(name, telList, nameList) )
+    print( nameToTels('Clau', telList, nameList) == ['777888333'] )
+    print( nameToTels('Br', telList, nameList) == ['234000111', '911911911'] )
+
+
+if __name__ == "__main__":
+    main()
