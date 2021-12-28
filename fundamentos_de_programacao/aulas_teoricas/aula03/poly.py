9e225e0738aa1dd4fd4496d5b12ba3823684f02c
Miguel Vila <miguelovila@ua.pt>
2021-12-28 06:07:30 +0000

    Committing 8 files

---
+++ fundamentos_de_programacao/aulas_teoricas/aula03/poly.py
@@ -0,0 +1,21 @@
+
+# Esta função implementa g(x) = 8 - x³
+def g(x):
+    return 8 - x**3
+
+# Defina uma função que implemente p(x) = x² + 2x + 3
+...
+
+def main():
+    # Mostra alguns valores da função g:
+    print("g(1) =", g(1))
+    print("g(2) =", g(2))
+    print("g(10) =", g(10))
+
+    # Acrescente instruções para mostrar os valores de
+    # p(1), p(2), p(10) e g(1 + p(3)).
+    ...
+
+if __name__ == '__main__':
+    main()
+
