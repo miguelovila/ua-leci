8c776d3285c5921971c47cbbcd260f0415718ad2
Miguel Vila <miguelovila@ua.pt>
2023-01-03 17:06:31 +0000

    Committing 10 files

---
+++ arquitetura_de_computadores_2/aulas_praticas/pratical_8/part_1/question_3/program.c
@@ -0,0 +1,23 @@
+#include <detpic32.h>
+
+int main(void) {
+    T3CONbits.TCKPS = 7;
+    PR3 = 39062;
+    TMR3 = 0;
+    T3CONbits.TON = 1;
+
+    IPC3bits.T3IP = 2;
+    IEC0bits.T3IE = 1;
+    IFS0bits.T3IF = 0;
+
+    EnableInterrupts();
+    while (1) { };
+    return 0;
+}
+
+void _int_(_TIMER_3_VECTOR) isr_T3(void) {
+    static char skip = 0;
+    if (!skip) putChar('.');
+    skip = !skip;
+    IFS0bits.T3IF = 0;
+}
