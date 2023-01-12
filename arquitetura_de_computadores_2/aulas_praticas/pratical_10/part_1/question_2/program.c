6f5884e171ca5d3eaf1024938f44d93f12095b0b
Miguel Vila <miguelovila@ua.pt>
2023-01-12 01:44:11 +0000

    Committing 10 files

---
+++ arquitetura_de_computadores_2/aulas_praticas/pratical_10/part_1/question_2/program.c
@@ -0,0 +1,35 @@
+#include <detpic32.h>
+
+void putc(char byte) 
+{ 
+    while (U2STAbits.UTXBF == 1) { }
+    U2TXREG = byte;
+}
+
+int main (void) {
+    U2BRG = 10;
+    U2MODEbits.BRGH = 0;
+
+    U2MODEbits.PDSEL = 00;
+    U2MODEbits.STSEL = 0;
+
+    U2STAbits.UTXEN = 1;
+    U2STAbits.URXEN = 1;
+    U2MODEbits.ON = 1;
+
+    while (1) {
+        putc('+');
+        resetCoreTimer();
+        while (readCoreTimer() <= 20000000) { }
+    }
+    return 0;
+}
+
+/*
+// Configuração do Baud Generator
+// baudrate = 115200 = 20000000/16*(U2BRRG + 1)
+//
+// assim,
+//
+// U2BRG = ((20000000/115200)/16) - 1 = 10
+*/
---
+++ arquitetura_de_computadores_2/aulas_praticas/pratical_10/part_1/question_3/program.c
@@ -0,0 +1,41 @@
+#include <detpic32.h>
+
+void putc(char byte) 
+{ 
+    while (U2STAbits.UTXBF == 1) { }
+    U2TXREG = byte;
+}
+
+void putstr(char * str) {
+    for (; *str != '\0'; str++) {
+        putc(*str);
+    }
+}
+
+int main (void) {
+    U2BRG = 10;
+    U2MODEbits.BRGH = 0;
+
+    U2MODEbits.PDSEL = 00;
+    U2MODEbits.STSEL = 0;
+
+    U2STAbits.UTXEN = 1;
+    U2STAbits.URXEN = 1;
+    U2MODEbits.ON = 1;
+
+    while (1) {
+        putstr("String de teste\n");
+        resetCoreTimer();
+        while (readCoreTimer() <= 20000000) { }
+    }
+    return 0;
+}
+
+/*
+// Configuração do Baud Generator
+// baudrate = 115200 = 20000000/16*(U2BRRG + 1)
+//
+// assim,
+//
+// U2BRG = ((20000000/115200)/16) - 1 = 10
+*/
