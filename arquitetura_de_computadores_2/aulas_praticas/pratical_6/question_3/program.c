a5da2f190f654e09b86c168591ba305db2305ed8
Miguel Vila <miguelovila@ua.pt>
2023-05-02 11:09:13 +0100

    Committing 10 files

---
+++ arquitetura_de_computadores_2/aulas_praticas/pratical_6/question_3/program.c
@@ -0,0 +1,28 @@
+#include <detpic32.h>
+
+int main(void) {
+    TRISBbits.TRISB4 = 1;
+    AD1PCFGbits.PCFG4 = 0;
+    AD1CON1bits.SSRC = 7;
+    AD1CON1bits.CLRASAM = 1;
+    AD1CON3bits.SAMC = 16;
+    AD1CON2bits.SMPI = 0;
+    AD1CHSbits.CH0SA = 4;
+    AD1CON1bits.ON = 1;
+
+    while (1) {
+        AD1CON1bits.ASAM = 1;
+        while (IFS1bits.AD1IF == 0);
+        int *p = (int *)(&ADC1BUF0);
+        int i = 0;
+        for( i = 0; i < 16; i++ ) {   // Dúvida
+            printInt( p[i*4], 16 | 3 << 16);
+            putChar(' ');
+        }
+        putChar('\n');
+        IFS1bits.AD1IF = 0;
+        AD1CON1bits.ASAM = 0;
+    }
+    
+    return 0;
+}
